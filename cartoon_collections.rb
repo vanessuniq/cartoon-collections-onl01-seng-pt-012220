a7759eff0d35e3aa1348629de29e6f6bd43b9786
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-01 17:56:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves array
 end
 
 def summon_captain_planet planeteer_calls 
-  array.map do |planeteer|
+  planeteer_calls.map do |planeteer|
     "#{planeteer.capitalize}!"
   end 
 end
