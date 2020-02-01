fe734fc3338ff31a7c60d3177f02710123b3e62b
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-01 18:27:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,7 @@ def summon_captain_planet planeteer_calls
 end
 
 def long_planeteer_calls calls 
-  calls.any? do |call|
-    (call.length > 4)?
-  end 
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese strings_array 
